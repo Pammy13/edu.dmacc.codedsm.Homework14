dcf5d2e6f292041482671eedfecbdc19b8a5dde7
unknown <pjhunter@dmacc.edu>
2019-03-31 15:47:44 -0500

    HW14dog

--- src/edu/dmacc/code/dsm/Homework14/DogGroomingShop.java
+++ src/edu/dmacc/code/dsm/Homework14/DogGroomingShop.java
@@ -4,61 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DogGroomingShop {
-
     private Object packages;
     private Object employees;
     private Object supplies;
 
 
-    DogGroomingShop(List<String> packages) {
+    @Override
+    public String toString() {
+        return "DogGroomingShop{" +
+                "packages=" + packages +
+                ", employees=" + employees +
+                ", supplies=" + supplies +
+                '}';
+    }
+
+    public Object getPackages() {
+        return packages;
+    }
+
+    public void setPackages(Object packages) {
         this.packages = packages;
     }
 
-    public void setEmployees(List<String> employees) {
+    public Object getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Object employees) {
         this.employees = employees;
     }
 
-    public void setSupplies(List<String> supplies) {
+    public Object getSupplies() {
+        return supplies;
+    }
+
+    public void setSupplies(Object supplies) {
         this.supplies = supplies;
     }
-
-    public DogGroomingShop() {
-        this.packages = new ArrayList();
-        this.employees = new ArrayList();
-        this.supplies = new ArrayList();
+}
 
 
-        public void setPackages (Object packages){
-            this.packages = packages;
-        }
-
-        public Object getPackages () {
-            return packages;
-        }
-
-        public Object getEmployees () {
-            return employees;
-        }
-
-        public void setEmployees (Object employees){
-            this.employees = employees;
-        }
-
-        public Object getSupplies () {
-            return supplies;
-        }
-
-        public void setSupplies (Object supplies){
-            this.supplies = supplies;
-        }
-
-        @Override
-        public String toString () {
-            return "DogGroomingShop{" +
-                    "packages=" + packages +
-                    ", employees=" + employees +
-                    ", supplies=" + supplies +
-                    '}';
-        }
-
-    }
--- src/edu/dmacc/code/dsm/Homework14/Runner.java
+++ src/edu/dmacc/code/dsm/Homework14/Runner.java
@@ -1,16 +1,31 @@
 package edu.dmacc.code.dsm.Homework14;
 
+import java.lang.reflect.Array;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Arrays;
+
+import static java.util.Arrays.*;
+
 public class Runner {
 
+    private static Object PersonAnimal;
+
     public static void main(String[] args) {
+
         DogGroomingShop Object1 = new DogGroomingShop();
         FullPackage Object2 = new FullPackage();
         QuickPackage Object3 = new QuickPackage();
         PersonAnimal Object4 = new PersonAnimal();
 
+        System.out.println("DogGroomingShop");
+        System.out.println("FullPackage");
+        System.out.println("QuickPackage");
+        System.out.println("PersonAnimal");
+
     }
 }
+
 
 
 
